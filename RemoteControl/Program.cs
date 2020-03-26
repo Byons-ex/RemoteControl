6d7d6542011c4bb428967a9f3dca599de9cbf33b
ppuding <buyongshi15@163.com>
2020-03-26 16:57:02 +0800

    null

--- RemoteControl/Program.cs
+++ RemoteControl/Program.cs
@@ -4,6 +4,8 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
 
 namespace RemoteControl
 {
@@ -11,38 +13,102 @@ namespace RemoteControl
     {
         static void Main(string[] args)
         {
-            switch(args[0].ToLower())
+            try
             {
-                case "shutdown":
-                    string host = null;
-                    string user = null;
-                    string password = null;
-                    for (var i = 1; i != 6; ++i)
-                    {
-                        switch(args[i].ToLower())
+                switch (args[0].ToLower())
+                {
+                    case "shutdown":
+                        string host = null;
+                        string user = null;
+                        string password = null;
+                        for (var i = 1; i != args.Length - 1; ++i)
                         {
-                            case "-a":
-                                host = args[i + 1];
-                                break;
-                            case "-u":
-                                user = args[i + 1];
-                                break;
-                            case "-p":
-                                password = args[i + 1];
-                                break;
-                            default:
-                                break;
+                            switch (args[i].ToLower())
+                            {
+                                case "-h":
+                                    host = args[i + 1];
+                                    break;
+                                case "-u":
+                                    user = args[i + 1];
+                                    break;
+                                case "-p":
+                                    password = args[i + 1];
+                                    break;
+                                default:
+                                    break;
+                            }
                         }
-                    }
-                    Shutdown(host, user, password);
-                    break;
+                        Shutdown(host, user, password);
+                        break;
 
-                case "startup":
-                    break;
-                case "help":
-                    break;
-                default:
-                    break;
+                    case "startup":
+                        string mac = null;
+                        string broadcastAddress = null;
+                        for (var i = 1; i != args.Length - 1; ++i)
+                        {
+                            switch (args[i])
+                            {
+                                case "-m":
+                                    mac = args[i + 1];
+                                    break;
+                                case "-b":
+                                    broadcastAddress = args[i + 1];
+                                    break;
+                            }
+                        }
+                        WakeOnLan(mac, broadcastAddress);
+                        break;
+
+                    case "help":
+                        throw new NotImplementedException();
+                    default:
+                        throw new ArgumentException();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.WriteLine("finish!");
+            Console.ReadLine();
+        }
+
+        static void WakeOnLan(string mac, string broadcastAddress = null)
+        {
+            if (mac.Length != 17)
+                throw new ArgumentException("提供的mac地址无效，它应该是类似FF:FF:FF:FF:FF:FF这样的格式");
+
+            byte[] macByte = new byte[6];
+
+            try
+            {
+                for (int i = 0; i < 6; ++i)
+                    macByte[i] = Convert.ToByte(mac.Substring(i * 3, 2), 16);
+            }
+            catch (Exception)
+            {
+                throw new ArgumentException("提供的mac地址无效，它应该是类似FF:FF:FF:FF:FF:FF这样的格式");
+            }
+
+            if (broadcastAddress == null)
+                broadcastAddress = "255.255.255.255";
+
+            IPAddress ip = null;
+            if(IPAddress.TryParse(broadcastAddress, out ip) == false)
+                throw new ArgumentException("提供的广播地址无效");
+
+            byte[] packet = new byte[6 + macByte.Length * 16];
+            for (int i = 0; i != 6; ++i)
+                packet[i] = 0xFF;
+
+            for (int i = 6; i <= packet.Length - 6; i += 6)
+                for (int j = 0; j != macByte.Length; ++j)
+                    packet[i + j] = macByte[j];
+
+            using(UdpClient udp = new UdpClient())
+            {
+                udp.Send(packet, packet.Length, new IPEndPoint(ip, 0));
             }
         }
 
@@ -58,6 +124,11 @@ namespace RemoteControl
 
             p.Start();
             p.ErrorDataReceived += (object sender, DataReceivedEventArgs e) => 
+            {
+                Console.WriteLine(e.Data);
+            };
+
+            p.OutputDataReceived += (object sender, DataReceivedEventArgs e) =>
             {
                 Console.WriteLine(e.Data);
             };
